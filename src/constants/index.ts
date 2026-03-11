3b3b41ec0450e97ee1aeeac523e39beac11ace2e
Victor Oladimeji <oladimejivictor611@gmail.com>
2026-03-11 21:13:03 +0100

    fixing issues

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,3 +1,4 @@
 // export const BASE_URL = "https://izabi.onrender.com";
+console.log('VITE_API_BASE_URL =', import.meta.env.VITE_API_BASE_URL);
 export const BASE_URL =
   import.meta.env.VITE_API_BASE_URL ?? 'http://localhost:3000';
